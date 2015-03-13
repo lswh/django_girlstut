46a50934301ce77972ffeb807602c46996c0b5bf
Helen Mary Labao <labao.hm@gmail.com>
2015-03-13 22:39:10 +0800

    wsgi and setting changes for heroku deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -8,9 +8,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# Import for HEROKU HERE
+import dj_database_url
 
 
 # Quick-start development settings - unsuitable for production
@@ -82,3 +86,19 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+DATABASES['default'] =  dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
