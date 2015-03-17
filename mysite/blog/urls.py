88c8001d1f97700658091a3267a5bd224e55236d
Helen Mary Labao <labao.hm@gmail.com>
2015-03-17 22:25:53 +0800

    publish button draft listing view and delete post on UI

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
+	url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
+	url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
+	url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
 	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
