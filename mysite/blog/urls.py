d1a52a3cffce013ee0391f715d0ee3a92c255c6e
Helen Mary Labao <labao.hm@gmail.com>
2015-03-17 21:40:16 +0800

    post form initial

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
+	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
     url(r'', views.post_list),
 )
